2a9e9d935bf4cd1b94d5113b8810f984d6476926
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2021-11-13 00:51:15 -0500

    add sass

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,18 @@ module.exports = {
                     // references .babelrc
                     loader: 'babel-loader',
                 }
-            }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+              },
         ]
     },
     devtool: 'source-map',
