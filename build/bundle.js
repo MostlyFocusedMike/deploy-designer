14ba34e26543a958372d4f21c1cb612158d09c74
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2021-11-12 22:26:31 -0500

    init

---
+++ build/bundle.js
@@ -0,0 +1,2 @@
+console.log("hi there"),console.log("hi therdasdasde"),console.log("new:asdasds ");
+//# sourceMappingURL=bundle.js.map
---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log('hi there')
+console.log('hi therdasdasde')
+console.log('new:asdasds ', );
+const obj = {a: 'a'};
+const newObj = {...obj, b: 'b'};
