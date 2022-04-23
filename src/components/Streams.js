fa7e0ea2ee6e320cb68feb64a9be882ba54ead55
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2022-04-23 17:36:00 -0400

    add new stream name

--- src/components/Streams.js
+++ src/components/Streams.js
@@ -3,29 +3,33 @@ import mainAdapter from '../adapters/main';
 import Stream from './Stream';
 
 const Streams = ({startingStreams, phaseId}) => {
-    const [streams, setStreams] = useState(startingStreams);
+  const [streams, setStreams] = useState(startingStreams);
+  const [newStreamName, setNewStreamName] = useState('')
+  const handleStreamName = (e) => {
+    setNewStreamName(e.target.value);
+  }
 
-    const handleAddStream = async (e) => {
-        e.preventDefault();
-        console.log('phaseId: ', phaseId);
-        const newStream = await mainAdapter.createStream('test', phaseId);
-        setStreams(streams => [...streams, newStream])
+  const handleAddStream = async (e) => {
+    e.preventDefault();
+    const newStream = await mainAdapter.createStream(newStreamName, phaseId);
+    setStreams(streams => [...streams, newStream])
+    setNewStreamName('');
+  }
+
+  return <div id='streams-container'>
+    <ol id='streams'>
+    {
+      streams && streams.map(stream => {
+        return <Stream stream={stream} key={stream.id} />
+      })
     }
-
-    return <div id='streams-container'>
-        <ol id='streams'>
-        {
-            streams && streams.map(stream => {
-                return <Stream stream={stream} key={stream.id} />
-            })
-        }
-        </ol>
-        <form>
-            <label htmlFor='stream-title'>Stream Title</label>
-            <input id='stream-title'/>
-            <button onClick={handleAddStream}>Create Stream</button>
-        </form>
-    </div>
+    </ol>
+    <form>
+      <label htmlFor='stream-title'>Stream Title</label>
+      <input onChange={handleStreamName} value={newStreamName} id='stream-title'/>
+      <button onClick={handleAddStream}>Create Stream</button>
+    </form>
+  </div>
 }
 
 export default Streams;
